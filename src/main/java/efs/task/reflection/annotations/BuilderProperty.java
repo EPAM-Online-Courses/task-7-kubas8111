718fc7e3c715d0400628051fadadb03bf96c41aa
kubas8111 <80070461+kubas8111@users.noreply.github.com>
2023-06-05 00:34:28 +0200

    Add files via upload

---
+++ src/main/java/efs/task/reflection/annotations/BuilderProperty.java
@@ -0,0 +1,10 @@
+package efs.task.reflection.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.*;
+
+@Retention(RetentionPolicy.SOURCE)
+@Target({ElementType.METHOD,ElementType.CONSTRUCTOR})
+public @interface BuilderProperty {
+    String name();
+}
